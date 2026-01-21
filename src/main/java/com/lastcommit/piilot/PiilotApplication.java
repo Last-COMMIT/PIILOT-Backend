745fa854b17e042f2338149095695806af8ce294
chanudevelop <chanu.develop@gmail.com>
2026-01-21 14:34:30 +0900

    [Feat] #1 프로젝트 기초 코드 세팅

---
+++ src/main/java/com/lastcommit/piilot/PiilotApplication.java
@@ -0,0 +1,13 @@
+package com.lastcommit.piilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lastcommit/piilot/PiilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastcommit.piilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
