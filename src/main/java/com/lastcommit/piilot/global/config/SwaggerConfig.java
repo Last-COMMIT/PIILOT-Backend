66b76457e5d431f22771686cb96a18c46f4d1fa1
chanudevelop <chanu.develop@gmail.com>
2026-01-21 15:19:36 +0900

    [Feat] #1 Swagger 설정

---
+++ src/main/java/com/lastcommit/piilot/global/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.lastcommit.piilot.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI PiilotAPI(){
+        Info info = new Info()
+                .title("PIILOT Backend API")
+                .description("개인정보보호 플랫폼 PIILOT Backend API 명세서")
+                .version("1.0.0");
+
+        String jwtSchemeName = "JWT TOKEN";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
