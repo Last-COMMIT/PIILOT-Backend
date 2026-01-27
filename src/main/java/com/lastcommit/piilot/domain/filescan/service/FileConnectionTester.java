ff7579353bf9710a7a9f6f6cca1d4bc3ed61aa54
chanudevelop <chanu.develop@gmail.com>
2026-01-27 13:55:37 +0900

    [Feat]#9 파일서버연결API- FTP 로그인 검증 추가

--- src/main/java/com/lastcommit/piilot/domain/filescan/service/FileConnectionTester.java
+++ src/main/java/com/lastcommit/piilot/domain/filescan/service/FileConnectionTester.java
@@ -3,11 +3,11 @@ package com.lastcommit.piilot.domain.filescan.service;
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.Socket;
 
 @Component
 public class FileConnectionTester {
@@ -24,11 +24,30 @@ public class FileConnectionTester {
     }
 
     private boolean testFtpConnection(String host, Integer port, String username, String password) {
-        try (Socket socket = new Socket()) {
-            socket.connect(new InetSocketAddress(host, port), CONNECTION_TIMEOUT_MS);
-            return socket.isConnected();
+        FTPClient ftpClient = new FTPClient();
+        try {
+            ftpClient.setConnectTimeout(CONNECTION_TIMEOUT_MS);
+            ftpClient.connect(host, port);
+
+            int replyCode = ftpClient.getReplyCode();
+            if (!FTPReply.isPositiveCompletion(replyCode)) {
+                return false;
+            }
+
+            boolean loginSuccess = ftpClient.login(username, password);
+            if (loginSuccess) {
+                ftpClient.logout();
+            }
+            return loginSuccess;
         } catch (IOException e) {
             return false;
+        } finally {
+            if (ftpClient.isConnected()) {
+                try {
+                    ftpClient.disconnect();
+                } catch (IOException ignored) {
+                }
+            }
         }
     }
 
