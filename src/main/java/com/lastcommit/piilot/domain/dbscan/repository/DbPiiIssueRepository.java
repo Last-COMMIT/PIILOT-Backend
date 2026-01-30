b22ef44ff8a667dcf65939a3a867ad7a76ac8f36
chanudevelop <chanu.develop@gmail.com>
2026-01-30 16:12:38 +0900

    [Add]#25 DB 이슈 Repository 구현 (QueryDSL)

--- src/main/java/com/lastcommit/piilot/domain/dbscan/repository/DbPiiIssueRepository.java
+++ src/main/java/com/lastcommit/piilot/domain/dbscan/repository/DbPiiIssueRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface DbPiiIssueRepository extends JpaRepository<DbPiiIssue, Long> {
+public interface DbPiiIssueRepository extends JpaRepository<DbPiiIssue, Long>, DbPiiIssueCustomRepository {
 
     Optional<DbPiiIssue> findByDbPiiColumnIdAndIssueStatus(Long columnId, IssueStatus issueStatus);
 }
