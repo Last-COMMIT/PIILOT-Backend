1ce4dab907128874accae6b88b67f33b99f8636e
chanudevelop <chanu.develop@gmail.com>
2026-02-04 20:37:42 +0900

    [Feat]#53 Document 엔티티 수정 및 DocumentType 변경

--- src/main/java/com/lastcommit/piilot/domain/document/entity/DocumentType.java
+++ src/main/java/com/lastcommit/piilot/domain/document/entity/DocumentType.java
@@ -1,7 +1,6 @@
 package com.lastcommit.piilot.domain.document.entity;
 
 public enum DocumentType {
-    LAWS,                  // 법령
-    INTERNAL_REGULATIONS,  // 내부규정
-    DB_INFO                // DB 정보
+    REGULATION,      // 법규 (법령 + 내부규정 통합)
+    DB_DICTIONARY    // DB 용어 사전
 }
